0ffd1f49e02ffb814baaf0c1c5adeb843949b1d2
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-06-27 02:16:59 +0100

    IOS project created

---
+++ WeTalk/WeTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeTalk
+//
+//  Created by Decagon on 27/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
